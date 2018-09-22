e39f736050825b857e07ea658119a6de367bf4f8
Brandon Pitzen <bmpitzen@icloud.com>
2018-09-22 15:01:08 -0400

    Seed data working

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -1,24 +1,22 @@
 class Api::UsersController < ApplicationController
+  skip_before_action :verify_authentication
 
-    skip_before_action :verify_authentication
+  def new
+    @user = User.new
+  end
 
-    def new
-        @user = User.new
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors
     end
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            render json: @user, status: :created
-        else
-            render json: @user.errors
-        end
-    end
+  private
 
-
-    private
-    
-        def user_params
-            params.permit(:name, :username, :password)
-        end
+  def user_params
+    params.permit(:name, :username, :password, :admin)
+  end
 end
