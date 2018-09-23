817c43def43347f59f3e44900322fb90cd3122b5
spatel1009 <spatel1009@gmail.com>
2018-09-23 17:13:51 -0400

    answercontroller working

--- backend/app/controllers/api/answers_controller.rb
+++ backend/app/controllers/api/answers_controller.rb
@@ -1,9 +1,15 @@
 class Api::AnswersController < ApplicationController
 
     before_action :verify_authentication
-    before_action :admin_user
+    before_action :admin_user, only: [:create, :update, :destroy]
 
 
+    def index
+        @question = Question.find(params[:question_id])
+        @answers = Answer.where(:question_id => @question.id)
+        render json: @answers
+    end
+    
     def create
         @question = Question.find(params["question_id"])
         @answer = Answer.new(answer_params)
@@ -14,13 +20,35 @@ class Api::AnswersController < ApplicationController
             end
     end
 
+    def show
+        
+        @answer = Answer.find(params[:id])
+        render json: @answer
+    end
+
+    def update
+        @answer = Answer.find(params[:id])
+        if @answer.update(answer_params)
+            render json: @answer, status: :created
+        else
+            render json: @answer.errors, status: :unprocessable_entity
+        end
+    end
+
+
     def destroy
-        @answer.destroy
+        @answer = Answer.find(params[:id])
+        if @answer.destroy
+            render json: { status: 'Deleted answer, yo'}
+        else
+            render json: @answer.errors, status: :unprocessable_entity
+        end
     end
 
     private
 
     def answer_params
         params.permit(:question_id, :a_text, :correct)
+    end
 
 end
