fa06606a7189470083a8100a9aab23c6c56b1f3c
Brandon Pitzen <bmpitzen@icloud.com>
2018-09-24 16:48:13 -0400

    Fixed formatting in quizzes controller.

--- backend/app/controllers/api/quizzes_controller.rb
+++ backend/app/controllers/api/quizzes_controller.rb
@@ -1,59 +1,51 @@
 class Api::QuizzesController < ApplicationController
-    before_action :verify_authentication
-    before_action :admin_user, only: [:create, :destroy, :update]
-    
-    
-    def index
+  before_action :verify_authentication
+  before_action :admin_user, only: %i[create destroy update]
+
+  def index
+    if admin_user
       @quizzes = Quiz.all
-      if admin_user 
-        render :index
-      else
-        @quizzes = @quizzes.where(published:true)
-        render :index
-      end
+    else
+      @quizzes = Quiz.where(published: true)
     end
+    render :index
+  end
 
-    def show
-        @quiz = Quiz.find(params[:id])
-        # render json: { id: @quiz.id, title: @quiz.title,
-        #   questions: @quiz.questions.all, answers: questions.answers.all }
-        render :show
-    end
+  def show
+    @quiz = Quiz.find(params[:id])
+    render :show
+  end
 
-    def create
-        @quiz = Quiz.new(quiz_params)
-        if @quiz.save 
-           render json: @quiz, status: :created
-        else 
-            render json: @quiz.errors, status: :unprocessable_entity
-        end
-    end
-    
-    def update 
-      @quiz = Quiz.find(params[:id])
-      if @quiz.update(quiz_params)
+  def create
+    @quiz = Quiz.new(quiz_params)
+    if @quiz.save
       render json: @quiz, status: :created
-      else 
-        render json: @quiz.errors, status: :unprocessable_entity
-      end
+    else
+      render json: @quiz.errors, status: :unprocessable_entity
     end
-    
-    def destroy
-        @quiz = Quiz.find(params[:id])
-        if @quiz.destroy
-          render json: { status: 'Deleted, yo'}
-        else
-          render json: @quiz.errors, status: :unprocessable_entity
-        end
-    end
+  end
 
+  def update
+    @quiz = Quiz.find(params[:id])
+    if @quiz.update(quiz_params)
+      render json: @quiz, status: :created
+    else
+      render json: @quiz.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @quiz = Quiz.find(params[:id])
+    if @quiz.destroy
+      render json: { status: 'Deleted, yo' }
+    else
+      render json: @quiz.errors, status: :unprocessable_entity
+    end
+  end
 
   private
-    def quiz_params
-        params.require(:quiz).permit(:title, :published, :questions)
-    end
 
-
-
-
+  def quiz_params
+    params.require(:quiz).permit(:title, :published, :questions)
+  end
 end
