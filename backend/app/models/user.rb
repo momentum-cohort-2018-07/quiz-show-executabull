196ced3d456981b2520b4f38667970e1f54c69bb
Brandon Pitzen <bmpitzen@icloud.com>
2018-09-25 15:07:25 -0400

    User validates name now. Also added http statuses to login and user.

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_token
   has_secure_password
   has_many :scores
+  validates :name, presence: true
   validates :username, uniqueness: true, length: {minimum: 4}
   validates :password, presence: true, length: {minimum: 2}
 end
