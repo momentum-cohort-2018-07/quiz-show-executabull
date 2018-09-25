e25d40f5cb6ea4b7edc7cf159ca27cc6fc5e66b1
Brandon Pitzen <bmpitzen@icloud.com>
2018-09-25 11:01:57 -0400

    Added users index

--- backend/app/controllers/api/users_controller.rb
+++ backend/app/controllers/api/users_controller.rb
@@ -5,6 +5,11 @@ class Api::UsersController < ApplicationController
     @user = User.new
   end
 
+  def index
+    @users = User.all
+    render json: @users
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
