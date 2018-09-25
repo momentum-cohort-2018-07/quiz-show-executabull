bf2c51f74b50fcdac758c421285cb656d566c29a
Brandon Pitzen <bmpitzen@icloud.com>
2018-09-25 12:00:40 -0400

    Pseudo code for scores.

--- backend/app/controllers/api/scores_controller.rb
+++ backend/app/controllers/api/scores_controller.rb
@@ -1,31 +1,26 @@
 class Api::ScoresController < ApplicationController
-  
-  def show
-  end
-
+  def show; end
 
   def create
     @correct_count = 0
-    @submitted = params[:answer_id]
-    @submitted.each do |c|
-      if Answer.find(c).correct?
-        @correct_count += 1
-      end
+    @user.id = user_id
+    @quiz.id = quiz_id
+    @answers.each do
+      @correct_count += @answer.correct
     end
 
     Score.create!(
-      quiz_id: params[:quiz_id], 
-      user_id: current_user.id, 
+      quiz_id: params[:quiz_id],
+      user_id: current_user.id,
       score: @correct_count
-      
     )
 
-    render json: {"Ay, yo - your score is": @current_count}
+    render json: { "Ay, yo - your score is": @current_count }
   end
 
-  private 
+  private
+
   def scores_params
     params.permit(:answer_id)
   end
-
 end
